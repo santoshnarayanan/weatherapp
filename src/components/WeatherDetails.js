7df4b016f98e172396515f9637373d33430ab8bb
Santosh Narayanan <santosh33@gmail.com>
2023-10-06 21:45:37 +0200

    Styling weather Card

--- src/components/WeatherDetails.js
+++ src/components/WeatherDetails.js
@@ -16,6 +16,48 @@ function WeatherDetails() {
                         <div className="place">Mumbai, IN</div>
                     </div>
                 </div>
+                <div className="date">{new Date().toLocaleString()}</div>
+                <div className="extra-temp">
+                    <div className="temp-info-minmax">
+                        <div className="two-sided-section">
+                            <p>
+                                <i className={"wi wi-sunset"}></i>
+                            </p>
+                            <p className="extra-info-leftside">
+                                18:30 <br /> Sunset
+                            </p>
+                        </div>
+
+
+                        <div className="two-sided-section">
+                            <p>
+                                <i className={"wi wi-humidity"}></i>
+                            </p>
+                            <p className="extra-info-leftside">
+                                34 <br /> Humidity
+                            </p>
+                        </div>
+                    </div>
+                    <div className="temp-info-minmax">
+                        <div className="two-sided-section">
+                            <p>
+                                <i className={"wi wi-rain"}></i>
+                            </p>
+                            <p className="extra-info-leftside">
+                                67 <br /> Pressure
+                            </p>
+                        </div>
+
+                        <div className="two-sided-section">
+                            <p>
+                                <i className={"wi wi-strong-wind"}></i>
+                            </p>
+                            <p className="extra-info-leftside">
+                                123 <br /> Wind
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </article>
         </>
     );
