23a89b12028ec653fd602d3c8c58e8d0647660df
Santosh Narayanan <santosh33@gmail.com>
2023-10-06 21:15:34 +0200

    Creating WeatherCard

---
+++ src/components/WeatherDetails.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function WeatherDetails() {
+    return (
+        <>
+            <article className="widget">
+                <div className="weatherIcon">
+                    <i className="wi wi-day-sunny"></i>
+                </div>
+                <div className="weatherInfo">
+                    <div className="temperature">
+                        <span>23.99&deg;</span>
+                    </div>
+                    <div className="description">
+                        <div className="weatherCondition">sunny</div>
+                        <div className="place">Mumbai, IN</div>
+                    </div>
+                </div>
+            </article>
+        </>
+    );
+}
+
+export default WeatherDetails;
--- src/components/searchMain.js
+++ src/components/searchMain.js
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
-import  "./style.css";
+import "./style.css";
+import WeatherDetails from "./WeatherDetails";
 
 function SearchMain() {
-    const [searchCity, setSearchCity] = useState(''); 
+    const [searchCity, setSearchCity] = useState('');
     console.log(searchCity);
-  return (
-    <div className="wrap">
-        <div className="search">
-            <input type="search" placeholder="Search City" id="search" value={searchCity} onChange={(e) => {setSearchCity(e.target.value)}} />
-        </div>
-        <button className="searchButton">
-            search city
-        </button>
-    </div>
-  )
+    return (
+        <>
+            <div className="wrap">
+                <div className="search">
+                    <input type="search" placeholder="Search City" id="search" value={searchCity} onChange={(e) => { setSearchCity(e.target.value) }} />
+                </div>
+                <button className="searchButton">
+                    search city
+                </button>
+            </div>
+            <WeatherDetails />
+        </>
+
+    );
 }
 
 export default SearchMain;
